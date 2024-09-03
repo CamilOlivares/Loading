3a8dae9b2b6918d0af5ce4e70519b7b1277a55a6
Camilo Olivares <140129147+CamilOlivares@users.noreply.github.com>
2024-09-02 22:30:54 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html> <!-- Define el tipo de documento como HTML5 -->
+<html lang="es"> <!-- Indica que el idioma del contenido es español -->
+<head>
+    <meta charset="UTF-8"> <!-- Define la codificación de caracteres como UTF-8 -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Configura el viewport para un diseño responsivo -->
+    <title>Mi Portafolio</title> <!-- Título de la página que aparece en la pestaña del navegador -->
+    <!-- Enlaces a hojas de estilo CSS -->
+    <link rel="stylesheet" href="assets/css/loader.css"> <!-- Estilos para el "loader" de la página (ajustar ruta dependiendo de donde se tenga el css) -->
+    
+</head>
+<body>
+
+
+    <!-- Scripts para el cargador de la página (ajustar ruta dependiendo de donde se tenga el script) -->
+    <script src="js/loader.js"></script>
+
+</body>
+</html>
